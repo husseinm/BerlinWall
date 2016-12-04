c05ac71f7a222be371c193be2e492b94e3643232
Mahdi Hussein <m@hdihusse.in>
2016-12-04 12:49:41 -0500

    Added Custom Cursor

--- berlin_wall/src/main.cpp
+++ berlin_wall/src/main.cpp
@@ -5,47 +5,48 @@
 #include <map>
 #include <SFML/Graphics.hpp>
 
+#include "config.h"
 #include "StateManager.h"
 
 
 int main(int argc, char* argv[]) {
-  sf::VideoMode resolution = sf::VideoMode(800, 600);
+    sf::VideoMode resolution = sf::VideoMode::getDesktopMode();
 
-  // TODO: Get from Config file...
-  // 4:3 resolution = sf::VideoMode(1366, 768);
+    // TODO: Get from Config
+  auto mainWindow = std::make_shared<sf::RenderWindow>(resolution, "Berlin Wall");
+  mainWindow->setFramerateLimit(120);
+  mainWindow->setVerticalSyncEnabled(true);
+  mainWindow->setMouseCursorVisible(false);
+    sf::Image appIcon;
+    appIcon.loadFromFile(baseDir + "icon.png");
+  mainWindow->setIcon(128, 128, appIcon.getPixelsPtr());
 
-  sf::RenderWindow mainWindow(resolution, "Berlin Wall");
-  mainWindow.setFramerateLimit(120);
-  mainWindow.setVerticalSyncEnabled(true);
-  
-   //mainWindow.setIcon(unsigned int width, unsigned int height, const Uint8 *pixels)
-
-  StateManager* manager = StateManagerFactory::getManager(std::unique_ptr<sf::RenderWindow>(&mainWindow));
+  StateManager* manager = StateManagerFactory::getManager(mainWindow);
   manager->registerState(StateId::TitleState);
   manager->pushState(StateId::TitleState);
 
   float delta = 0.0;
 
-  while (mainWindow.isOpen()) {
-
-
+  while (mainWindow->isOpen()) {
     // Clear the screen
-    mainWindow.clear();
+    mainWindow->clear();
 
     // Handle Input - TODO: Move to thread
     sf::Event event;
 
-    while (mainWindow.pollEvent(event)) {
+    while (mainWindow->pollEvent(event)) {
       manager->handleEvent(event);
     }
-    /*while (MessageStack.pollMessage()) {
+    /*
+    while (MessageStack.pollMessage()) {
       manager->handleMessage(<#const Message &#>)
-      }*/
+    }
+    */
     manager->update(delta);
     manager->draw();
 
     // Flip the buffer
-    mainWindow.display();
+    mainWindow->display();
 
     //delta = Timer.reset();
   }
