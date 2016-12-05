dd3caddcc87b0889e6e306144ac62eda6790005f
Manu O <Manu O>
2016-12-04 23:12:58 -0500

    added some comments

--- berlin_wall/src/TitleState.cpp
+++ berlin_wall/src/TitleState.cpp
@@ -3,21 +3,37 @@
 #include "TitleState.h"
 #include "StateManager.h"
 
-TitleState::TitleState() : kbPlanetEarth(), background(sf::Vector2f(screenWidth, screenHeight)), quitButton(Button("Quit Game", 75, sf::Vector2f(screenWidth * 0.75, screenHeight * 0.2),
-                                                                                                                   sf::Vector2f(screenWidth * 0.5, screenHeight * 0.8), Message::CloseGame)) {
-  kbPlanetEarth.loadFromFile(baseDir + "KBPlanetEarth.ttf");
+TitleState::TitleState() : kbPlanetEarth(), 
+    background(sf::Vector2f(screenWidth, screenHeight)), 
+    quitButton(Button("Quit Game", 75, 
+    sf::Vector2f(screenWidth * 0.75, screenHeight * 0.2),
+    sf::Vector2f(screenWidth * 0.5, screenHeight * 0.8), Message::CloseGame)) 
+    {
+  
+        // Load font
+    kbPlanetEarth.loadFromFile(baseDir + "KBPlanetEarth.ttf");
+        
+        // Set the title font
   mainHeader = sf::Text("Berlin Wall", kbPlanetEarth, 180);
+            
+        // Make the title bold
   mainHeader.setStyle(sf::Text::Bold);
+        
+        // Set the title color to black
   mainHeader.setFillColor(sf::Color::Black);
 
   // Title
+    // Set the origin of the header to the center
   sf::FloatRect headerRect = mainHeader.getLocalBounds();
   mainHeader.setOrigin(headerRect.left + headerRect.width / 2.0f,
       headerRect.top + headerRect.height / 2.0f);
-
+        
+        // Set the position of the header
   mainHeader.setPosition(screenWidth * 0.5, screenHeight * 0.2);
 
   // Background
+        
+        // Load the image and set it to tile
   backgroundImage.loadFromFile(baseDir + "bg.jpg");
   backgroundImage.setRepeated(true);
   background.setTexture(&backgroundImage);
@@ -41,6 +57,7 @@ void TitleState::handleMessage(const Message&) {
 bool TitleState::update(float dt) {
 }
 
+// Render
 void TitleState::draw(sf::RenderWindow* context) {
   context->draw(background);
   context->draw(mainHeader);
